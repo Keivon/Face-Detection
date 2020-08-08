98e44556d2ff5b15c5a38dadfe22bbbd106b10b1
Keivon <keivonproctor@hotmail.com>
2020-08-08 01:39:59 +0100

    added timer

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from "react";
 import Webcam from "react-webcam";
-import './App.css';
+import "./App.css";
 
 function App() {
 
+ 
   const webcamRef = React.useRef(null);
+ 
    
   const videoConstraints = {
     width: 1280,
     height: 720,
     facingMode: "user"
   };
-  
-   
-  
+
     
-    const [imgSrc, setImgSrc] = React.useState(null);
-  
+    const [imgSrc, setImgSrc] = useState(null);
+    const [start, setStart] = useState(false);
+    const [counter, setCounter] = useState(10);
+    
     const capture = React.useCallback(() => {
       const imageSrc = webcamRef.current.getScreenshot();
       setImgSrc(imageSrc);
-    }, [webcamRef, setImgSrc]);
-    
+    }, [webcamRef, setImgSrc]);  
+
+    useEffect(() => {
+      if(start) {
+        counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
+      }
+
+    }, [start, counter]);
   
     return (
+      
       <div className="App">
         <Webcam
           audio={false}
-          height={720}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width={720}
           videoConstraints={videoConstraints}
+          className="video"
         />
+        <div className="overlay">
+        <div className="text">{counter}</div>
+        </div>
+        <button onClick={()=>{setStart(true)}}>Start</button>
        <button onClick={capture}>Capture photo</button>
       {imgSrc && (
         <img
@@ -41,6 +53,7 @@ function App() {
         />
       )}
         </div>
+        
     );
   
 }
